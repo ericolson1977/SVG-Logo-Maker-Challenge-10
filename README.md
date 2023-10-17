b560255e0d4ad920ce5985cf7675d0c6067b1155
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-16 23:35:53 -0500

    completed final functionality, ran tests and added a README file.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # SVG-Logo-Maker-Challenge-10
+![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+With the goal of being able to generate and simple logo for my projects, I created a command line SVG logo maker. After running node index.js in the terminal, the user will be prompted to choose up to 3 letters of text, the text color, the logo shape, and logo color. Once complete, the logo will be generated and shown as a 300x200 pixel image. 
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Credits](#credits)
+4. [Tests](#tests)
+5. [Questions](#questions)
+6. [License](#license)
+
+## Installation
+To install the necessary dependencies, run the following command:
+    
+    npm i
+
+## Usage
+n/a
+
+## Contributing
+I used npm inquirer, npm jest, npm color and npm fs. The test questions were provided by the instructional staff.
+
+## Tests
+To run tests, run the following command:
+    
+    npm run test
+
+## Questions
+If you have any questions about the repo, open an issue or contact me directly at mr_tues_night@yahoo.com. You can find more of my work at [ericolson1977](https://github.com/ericolson1977).
+
+## License
+  This project is licensed under the MIT license.
--- index.js
+++ index.js
@@ -1,60 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const color = require('color-name');
-const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-const generateSvg = require('./lib/shapesold.js');
+const { questions } = require('./lib/questions');
+const { generateSvg } = require('./lib/generatesvg');
 
-//function to validate use input as a recognizable color
-function validColor(input) {
-    if (hexColorRegex.test(input)) {
-        return true;
-    }
-    if (color[input.toLowerCase()]) {
-        return true;
-    }
-    return false;
-}
-// array of questions 
-const questions = [
-    {
-        type: 'input',
-        message: 'Enter text for the logo. (Must be no longer than 3 characters.)',
-        name: 'text',
-        validate: function (input) {
-            if (input.length > 3) {
-                return "Text must be 3 characters or less.";
-            } return true;
-        }
-    },
-    {
-        type: 'input',
-        message: 'Enter a text color',
-        name: 'textcolor',
-        validate: function (input) {
-            if (validColor(input)) {
-                return true;
-            } return "Must enter a valid color or hex color code.";
-        }
-    },
-    {
-        type: 'list',
-        message: 'Select a shape for the logo',
-        choices: ['circle', 'square', 'triangle'],
-        name: 'shape',
-    },
-    {
-        type: 'input',
-        message: 'Enter a shape color',
-        name: 'shapecolor',
-        validate: function (input) {
-            if (validColor(input)) {
-                return true;
-            } return "Must enter a valid color or hex color code.";
-        }
-    },
-];
-
-// fuction to start the command line question, this also grabs the data from the generateSvg function and finally creates the logo.svg file
+// This fuction starts the command line prompts, this also grabs the data from the generateSvg function and finally creates the logo.svg file.
 function init() {
     inquirer
         .prompt(questions)
@@ -70,5 +19,5 @@ function init() {
         });
 };
 
-// call the init function
+// Calling the init function.
 init();
